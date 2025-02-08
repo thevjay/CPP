039b0cf0155d85d46b1a141593a4d4fe3c195bf9
Vijay <rebalvijay930@gmail.com>
2025-02-08 23:43:40 +0530

    Binary Search

--- week1/sorting/BinarySearch.cpp
+++ week1/sorting/BinarySearch.cpp
@@ -4,13 +4,37 @@ using namespace std;
 
 
 int binarySearch(int arr[], int size, int target){
+    int start = 0;
+    int end = size-1;
 
+    int mid = (start+end)/2;
+
+    while (start <= end)
+    {
+        int element = arr[mid];
+
+        if (element == target)
+        {
+            return mid;
+        }
+        else if (target < element){
+            // left search 
+            end = mid - 1;
+        }
+        else{
+            // search in right
+            start = mid + 1;
+        }
+        mid = (start+end)/2 ;
+    }
+    // element not found
+    return -1;
 }
 
 int main(){
 
     int arr[] = { 2,4,6,8,10,12,16};
-    int size = 7;
+    int size = sizeof(arr)/sizeof(arr[0]);  //7
     int target = 2;
 
     int indexOftarget = binarySearch(arr,size,target);
@@ -19,9 +43,9 @@ int main(){
         cout << "target not found" << endl;
     }
     else{
-        cout << "target found at" << indexOftarget << endl;
+        cout << "target found at " << indexOftarget << " index " << endl;
     }
     return 0;
 }
 
-23
+58
