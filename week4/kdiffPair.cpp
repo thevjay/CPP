e1c1068558e90a23f26b7ececb66238685684a9a
Vijay <rebalvijay930@gmail.com>
2025-02-16 00:43:27 +0530

    K-diff of an Unique pairs an array

--- week4/kdiffPair.cpp
+++ week4/kdiffPair.cpp
@@ -43,6 +43,34 @@ int findPairs(int arr[],int n,int k){
 }
 
 //Binary Search
+ int bs(int arr[],int n,int start,int x){
+    int end = n-1;
+    while(start<=end){
+        int mid = (start+end)/2;
+        if(arr[mid] == x){
+            return mid;
+        }
+        else if(x > arr[mid]){
+            start = mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return -1;
+ }
+
+ int FindPairs(int arr[],int n,int k){
+    sort(arr,arr+n);
+    set<pair<int, int>> ans;
+
+    for(int i=0;i<n;i++){
+        if(bs(arr,n,i+1,arr[i]+k) != -1){
+            ans.insert({arr[i],arr[i]+k});
+        }
+    }
+    return ans.size();
+ }
 
 int main(){
     int arr[]={1,3,1,5,4};
@@ -51,7 +79,12 @@ int main(){
 
     // kdiffPair(arr,n,k);
 
-    int count = findPairs(arr,n,k);
-    cout<<"\nCount"<<count<<endl;
+    // int count = findPairs(arr,n,k);
+    // cout<<"\nCount"<<count<<endl;
+
+    
+    int count = FindPairs(arr,n,k);
+    cout<<"\nCount: "<<count<<endl;
+
     return 0;
 }
